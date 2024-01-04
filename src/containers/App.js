e0bc47b6b9a1abead2bbb3bfbde127dc27277329
Jeremy Nelson <jeremynelson@Jeremys-MacBook-Pro.local>
2024-01-04 14:17:54 -0600

    update to use hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,38 +8,39 @@ import ErrorBoundry from '../components/ErrorBoundry'
 const App = () => {
     const onSearchChange = (event) => {
         setsearchfield(event.target.value);
-        //this.setState({searchfield: event.target.value})
     }
+    
+
+    const [robots, setRobots] = useState([])
+    const [searchfield, setsearchfield] = useState("")
+    
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response=>response.json())
             .then(users => setRobots(users))
 
-    })
-
-    const [robots, setRobots] = useState([])
-    const [searchfield, setsearchfield] = useState("")
+    }, [])
 
     const filteredRobots = robots.filter(robot => {
                     return robot.name.toLowerCase().includes(searchfield.toLowerCase())
                 })
     if(!robots.length){
-                    return <h1>Loading</h1>
-                }else{
-                    return (
-        
-                        <div className='tc'>
-                        <h1 className='f2'>RoboFriends!</h1>
-                        <SearchBox searchChange={onSearchChange} />
-                        <Scroll>
-                            <ErrorBoundry>
-                            <CardList robots={filteredRobots} />
-                            </ErrorBoundry>
-                        </Scroll>
-                        </div>
-        
-                    )
-                }
+        return <h1>Loading</h1>
+    }else{
+        return (
+
+            <div className='tc'>
+            <h1 className='f2'>RoboFriends!</h1>
+            <SearchBox searchChange={onSearchChange} />
+            <Scroll>
+                <ErrorBoundry>
+                <CardList robots={filteredRobots} />
+                </ErrorBoundry>
+            </Scroll>
+            </div>
+
+        )
+    }
 }
 
 
